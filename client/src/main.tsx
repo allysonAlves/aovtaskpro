91eea5a6ab3c7d8d528adbe0b13bf80c3151d199
Vagner H <vh3rd7@gmail.com>
2023-12-04 08:16:23 -0300

    update vitejs to typescript

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
+import App from './App.js'
 import './index.css'
 import Home from './pages/home/index.jsx'
 
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
   }  
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>    
       <RouterProvider router={router} />   
   </React.StrictMode>,
